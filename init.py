7c4a42e643caceadc1dd68f9c50c692b23e9e8e5
Maxim Vasyuta <ecratos12@gmail.com>
2017-06-26 02:44:56 +0300

    можно ебошить графики

--- init.py
+++ init.py
@@ -1,64 +1,75 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QLabel, QGridLayout,
-							 QAction, QMainWindow, QPushButton,
-							 QMessageBox, QFileDialog)
+from PyQt5.QtWidgets import (QApplication,
+							 QWidget,
+							 QLabel,
+							 QGridLayout,
+							 QAction,
+							 QMainWindow,
+							 QPushButton,
+							 QMessageBox,
+							 QFileDialog,
+							 QSizePolicy)
 from PyQt5.QtGui import QIcon
 # from PyQt5.QtCore import QCoreApplication
+import pandas as pd
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
-
-
+import random
 
 
 class Example(QMainWindow):
 
 	def __init__(self):
 		super().__init__()
+		self.interface = QWidget(
+			self)  # grid layout don't working with QMainWindow, so I attached all buttons, labels, etc.. to @interface
 		self.init_ui()
+		self.data = pd.read_csv('C:/Users/Максим Васюта/Desktop/new article/figures/3comp', delimiter=' ', header=None)
 
 	def init_ui(self):
-		# self.setToolTip('This is the <b>ELF</b> widget')
 		self.statusBar().showMessage('Ready')
+		menubar = self.menuBar()
+		fileMenu = menubar.addMenu('&File')
 
-		self.menubar = self.menuBar()
-		self.fileMenu = self.menubar.addMenu('&File')
+		openFile = QAction('Open', self)
+		openFile.setShortcut('Ctrl+O')
+		openFile.setStatusTip('Load data')
+		openFile.triggered.connect(self.load_file)
+		fileMenu.addAction(openFile)
 
-		self.openFile = QAction('Open', self)
-		self.openFile.setShortcut('Ctrl+O')
-		self.openFile.setStatusTip('Load data')
-		self.openFile.triggered.connect(self.load_file)
-		self.fileMenu.addAction(self.openFile)
+		exitAction = QAction('&Quit!1', self)
+		exitAction.setShortcut('Ctrl+Q')
+		exitAction.triggered.connect(self.closeEvent)
+		exitAction.setStatusTip('Leave the app')
+		fileMenu.addAction(exitAction)
 
-		self.exitAction = QAction('&Rage Quit!1', self)
-		self.exitAction.setShortcut('Ctrl+Q')
-		self.exitAction.triggered.connect(self.closeEvent)
-		self.exitAction.setStatusTip('Leave the app')
-		self.fileMenu.addAction(self.exitAction)
+		# ----------------------
+		# setting the @interface
+		# ----------------------
+		self.setCentralWidget(self.interface)
 
-		self.load_btn = QPushButton('Load File', parent=self)
-		self.res_btn = QPushButton('Do research!', parent=self)
-		self.res_btn.resize(self.res_btn.sizeHint())  # recommended size
-		self.load_btn.resize(self.res_btn.sizeHint())
-		self.res_btn.move(0, 50)
-		self.load_btn.move(0, 25)
+		self.interface.load_btn = QPushButton('Load File', self.interface)
+		self.interface.plot_btn = QPushButton('Plot data', self.interface)
+		self.interface.topFileName = QLabel(self.interface)
+		self.interface.result_log = QLabel(self.interface)
 
-		self.res_btn.clicked.connect(self.lesya_write_cod_function)
-		self.load_btn.clicked.connect(self.load_file)
+		self.grid = QGridLayout(self.interface)
+		self.grid.setSpacing(10)
+		self.grid.addWidget(self.interface.load_btn, 0, 0)
+		self.grid.addWidget(self.interface.plot_btn, 1, 0)
+		self.grid.addWidget(self.interface.topFileName, 0, 1)
+		self.grid.addWidget(self.interface.result_log, 1, 1)
+		self.setLayout(self.grid)
 
-		self.topFileName = QLabel(self)
-		self.topFileName.move(75, 30)
+		self.interface.plot_btn.clicked.connect(self.plot)
+		self.interface.load_btn.clicked.connect(self.load_file)
 
-		self.result_log = QLabel(self)
-		self.result_log.move(75, 55)
-
-		grid = QGridLayout(self)
-		grid.setSpacing(10)
-		grid.addWidget(self.load_btn, 0, 0)
-		grid.addWidget(self.res_btn, 1, 0)
-		grid.addWidget(self.topFileName, 0, 1)
-
-		self.setFixedSize(300, 240)
 		self.setWindowTitle('ELF Data GUI')
 		self.setWindowIcon(QIcon('icon.png'))
+		self.show()
+
 
 	def closeEvent(self, event):  # it is template QWidget function
 		reply = QMessageBox.question(self, 'Message',
@@ -70,23 +81,45 @@ class Example(QMainWindow):
 			event.ignore()
 
 	def load_file(self):
-		fname = QFileDialog.getOpenFileName(self, 'Load data')[0].split('/')
-		fname = fname[len(fname)-1]
+		fname = QFileDialog.getOpenFileName(self, 'Load data')[0]
 		if fname:
-			self.topFileName.setText('Loaded data file is <b>' + fname + '</b>')  #
-			self.topFileName.resize(self.topFileName.sizeHint())
-			with open(fname, 'r') as f:
-				pass
+			self.data = pd.read_csv(fname, delimiter=' ', header=None)
+			fname = fname.split('/')
+			fname = fname[len(fname) - 1]
+			self.interface.topFileName.setText('Loaded data file is <b>' + fname + '</b>')
+			self.interface.topFileName.resize(self.interface.topFileName.sizeHint())
 		else:
 			pass
 
-	def lesya_write_cod_function(self):
-		self.result_log.setText('<b>research done, congrats!</b>')
-		self.result_log.resize(self.result_log.sizeHint())
+	def plot(self):
+		# self.interface.result_log.setText('<b>research done, congrats!</b>')
+		# self.interface.result_log.resize(self.interface.result_log.sizeHint())
+		self.interface.graph = PlotCanvas(self.interface, width=5, height=4)
+		self.grid.addWidget(self.interface.graph, 0, 2, 3, 1)
+
+
+class PlotCanvas(FigureCanvas):
+	def __init__(self, parent=None, width=5, height=4, dpi=100):
+		fig = Figure(figsize=(width, height), dpi=dpi)
+		self.axes = fig.add_subplot(111)
+
+		FigureCanvas.__init__(self, fig)
+		self.setParent(parent)
+		FigureCanvas.setSizePolicy(self,
+								   QSizePolicy.Expanding,
+								   QSizePolicy.Expanding)
+		FigureCanvas.updateGeometry(self)
+		self.plot()
+
+	def plot(self):
+		data = [random.random() for i in range(25)]
+		ax = self.figure.add_subplot(111)
+		ax.plot(data, 'r-')
+		ax.set_title('PyQt Matplotlib Example')
+		self.draw()
 
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	ex = Example()
-	ex.show()
 	sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,21 +1,62 @@
-from PyQt5.QtWidgets import  QFileDialog, QWidget, QApplication, QHBoxLayout, QPushButton
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
+	QPushButton
+from PyQt5.QtGui import QIcon
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import random
 
 
-class Window(QWidget):
-    def __init__(self):
-        super(Window, self).__init__(None)
-        layout = QHBoxLayout()
-        self.button = QPushButton('click')
-        layout.addWidget(self.button)
-        self.setLayout(layout)
-        self.button.clicked.connect(self.ask_filename)
-    def ask_filename(self):
-        fname = QFileDialog.getSaveFileName(self, 'title')
-        print(fname)
-        self.button.setText(fname[0])
+class App(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.left = 10
+		self.top = 10
+		self.title = 'PyQt5 matplotlib example - pythonspot.com'
+		self.width = 640
+		self.height = 400
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle(self.title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
+
+		m = PlotCanvas(self, width=5, height=4)
+		m.move(0, 0)
+
+		button = QPushButton('PyQt5 button', self)
+		button.setToolTip('This s an example button')
+		button.move(500, 0)
+		button.resize(140, 100)
+
+		self.show()
 
 
-app = QApplication([])
-window = Window()
-window.show()
-app.exec_()
+class PlotCanvas(FigureCanvas):
+	def __init__(self, parent=None, width=5, height=4, dpi=100):
+		fig = Figure(figsize=(width, height), dpi=dpi)
+		self.axes = fig.add_subplot(111)
+		FigureCanvas.__init__(self, fig)
+		self.setParent(parent)
+		FigureCanvas.setSizePolicy(self,
+								   QSizePolicy.Expanding,
+								   QSizePolicy.Expanding)
+		FigureCanvas.updateGeometry(self)
+		self.plot()
+
+	def plot(self):
+		data = [random.random() for i in range(25)]
+		ax = self.figure.add_subplot(111)
+		ax.plot(data, 'r-')
+		ax.set_title('PyQt Matplotlib Example')
+		self.draw()
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
